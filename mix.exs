bbc053639a80cdd0949509466534b8e1215a05fb
Everett Griffiths <everett@fireproofsocks.com>
2022-03-10 14:52:09 -0500

    Changs License slug to Apache-2.0 for compatibility with https://spdx.org/licenses

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule Permutation.MixProject do
   defp package do
     [
       maintainers: ["Everett Griffiths"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       logo: "assets/logo.png",
       links: links(),
       files: [
